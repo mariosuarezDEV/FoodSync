c7f8363d1ffd9a4696453627c326c334012eb8f3
RodoH15 <rodoh20@outlook.com>
2025-03-12 19:34:15 +0000

    Agregar modelos y migraciones para categorías, grupos y productos; registrar modelos en el admin

--- grupo/admin.py
+++ grupo/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import ModelGrupo, ModelSubGrupo
 # Register your models here.
+
+admin.site.register(ModelGrupo)
+admin.site.register(ModelSubGrupo)
--- producto/admin.py
+++ producto/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import Producto as ModelProducto
 # Register your models here.
+
+admin.site.register(ModelProducto)
